750f445e3334f6a6e7d2dcff4ded4c36e762593f
bastamon <bastamon@sohu.com>
2018-12-12 20:08:16 +0800

    201812122008

--- dataanalyzer.m
+++ dataanalyzer.m
@@ -1,4 +1,4 @@
-clear;clc;
+clear;clc;close all;
 % [ref,temp] = xlsread('./leach-orig/MHLeach_orig_Results.xlsx','Results');
 % for i=1:length(ref)
 %     ref(i,5)=datenum(temp(i+1,5));
@@ -39,16 +39,42 @@ load('orig_simpled.mat');
 
 figure(1)
 simples(16)=[];
+msY=0;
 for i=1:length(simples)
     [m,~]=size(simples(i).nodeID);
     x=linspace(1,m,m)';
     y=simples(i).nodeID(:,6);
     plot(x,y);
     hold on;
-    
+    if msY<length(y)
+        msY=length(y);        
+    end
 end
 xlabel('时间');
 ylabel('电压');
+grid on;
+hold off;
+
+
+Ematrix=[];
+counts=[];
+for i=1:length(simples)
+    Ematrix=[Ematrix,[simples(i).nodeID(:,6);zeros(msY-length(simples(i).nodeID(:,6)),1)]];
+end
+for i=1:length(Ematrix)
+    counts=[counts;sum((Ematrix(i,:)~=0))];
+end
+
+
+AvgEn=sum(Ematrix,2)./counts;
+AvgEn=sort(AvgEn,'descend');
+figure(2)
+b=polyfit(x,AvgEn,6);%进行6次拟合，b是多项式前面的值。就如2次拟合中y=ax+b,a,b的值。
+yy=polyval(b,x);%得到拟合后y的新值
+plot(x,yy,'b-')%画拟合图 
+
+xlabel('时间');
+ylabel('平均电压');
 grid on;
 hold off;
 
