37b88238ce90f66fd5c5128805ce85f5591c982a
bastamon <bastamon@sohu.com>
2018-12-15 22:36:47 +0800

    第二次MHLeach-orig

--- dataanalyzer.m
+++ dataanalyzer.m
@@ -1,49 +1,94 @@
 clear;clc;close all;
-% [ref,temp] = xlsread('./leach-orig/MHLeach_orig_Results.xlsx','Results');
-% for i=1:length(ref)
-%     ref(i,5)=datenum(temp(i+1,5));
-% end
+% [ref,temp] = xlsread('./leach-orig/MHLeach_orig_Results','Results');
+% ref(1:end,5)=datenum(temp(2:end,5));
+% 
 % 
 % comp = xlsread('./leach-orig/MHLeach_orig_Results.xlsx','Compute');
 % save ( 'orig.mat','comp', 'ref');
 % clear;clc;
-% MINUTEUNIT=datenum(2001,01,01,12,01,00)-datenum(2001,01,01,12,00,00);
-% HOURUNIT=datenum(2001,01,01,13,00,00)-datenum(2001,01,01,12,00,00);
-% load('orig');
-% 
-% for i=1:length(comp)
-%     onenode = ref(ref(:,2)==comp(i,2),:);
-%     timespan = round((onenode(end,5)-onenode(1,5))/HOURUNIT);
-% %     idx=round(linspace(onenode(1,4),onenode(end,4),timespan));
-% %     idx=round(linspace(1,length(onenode),timespan));
-%     simple=onenode(1,:);
+MINUTEUNIT=datenum(2001,01,01,12,01,00)-datenum(2001,01,01,12,00,00);
+HOURUNIT=datenum(2001,01,01,13,00,00)-datenum(2001,01,01,12,00,00);
+load('orig1'); 
+
+timespan = 24*day(ref(end,5)-ref(1,5))+hour(ref(end,5)-ref(1,5));
+% for i=1:length(comp(:,2))       
+%     simple=zeros(timespan+1,8);
+%     if comp(i,2)==412||comp(i,2)==436
+%         load('orig');
+%         onenode = ref(ref(:,2)==comp(i,2),:);%412,436 
+%         load('orig1');
+%     else
+%         onenode = ref(ref(:,2)==comp(i,2),:);
+%     end
+%     simple(1,:)=onenode(1,:);
 %     lasttime=onenode(1,5);
-%     start=1;
-%     for j=1:timespan
-%         for k=start:length(onenode)
-%             if abs(hour(onenode(k,5))-hour(lasttime))>=hour(HOURUNIT)
-%                 simple=[simple;onenode(k,:)];
+%     start=2;
+%     for j=2:timespan
+%         for k=start:length(onenode(:,2))
+%             if abs(hour(onenode(k,5))-hour(lasttime))>=1
+%                 simple(j,:)=onenode(k,:);
 %                 lasttime=onenode(k,5);
 %                 start=k+1;
 %                 break;   
 %             end
 %         end
+%     end   
+%     j=length(simple(:,2));
+%     while(j)
+%         if sum(simple(j,:))~=0
+%             break;
+%         else
+%             simple(j,:)=[];   
+%         end
+%         j=j-1;
 %     end
-%     simple=[simple;onenode(end,:)];
+%     if i~=1&&i~=16
+%         simple1=zeros(timespan+1,8);
+%         simple1(1,:)=simple(1,:);
+%         for n=2:length(simple(:,2))
+%             simple1(1+24*day(simple(n,5))+hour(simple(n,5))-(24*day(simple(1,5))+hour(simple(1,5))),:)=simple(n,:);
+%         end
+%         n=length(simple1(:,1));
+%         while(n)
+%             if sum(simple1(n,:))~=0
+%                 break;
+%             else
+%                 simple1(n,:)=[];   
+%             end
+%             n=n-1;
+%         end
+%         simple=simple1;
+%     end
+%     
+%     simple=[simple;onenode(end,:)];    
 %     simples(i).nodeID=simple;
 % end
-% save ( 'orig_simpled.mat','comp', 'ref','simples');
+% 
+% 
+% 
+% 
+% save ( 'orig_simpled1.mat','comp', 'ref','simples');
 % clear;
-load('orig_simpled.mat');
+load('orig_simpled1.mat');
 
 
 figure(1)
-simples(16)=[];%去除异常
+% simples(16)=[];%去除异常
 msY=0;
 for i=1:length(simples)
     [m,~]=size(simples(i).nodeID);
-    x=linspace(1,m,m)';
-    y=simples(i).nodeID(:,6);
+    x=linspace(1,m,m)'-1;
+%     x1=linspace(1,m,m)';
+    y=simples(i).nodeID(:,6);    
+    x(all(y==0,2),:)=[];
+    x(end)=x(end-1)+minute(simples(i).nodeID(end,5))/60;
+    y(all(y==0,2),:)=[];
+    y=sort(y,'descend');
+%     z=interp1(x,y,x1);    
+%     b=polyfit(x,y,2);
+%     yy=polyval(b,x);
+    
+    
     plot(x,y);
     hold on;
     if msY<length(y)
@@ -67,11 +112,13 @@ end
 
 
 AvgEn=sum(Ematrix,2)./counts;
-AvgEn=sort(AvgEn,'descend');
 figure(2)
-b=polyfit(x,AvgEn,6);%进行6次拟合，b是多项式前面的值。就如2次拟合中y=ax+b,a,b的值。
-yy=polyval(b,x);%得到拟合后y的新值
-plot(x,yy,'b-')%画拟合图 
+x=linspace(1,length(AvgEn),length(AvgEn))'-1;
+AvgEn=sort(AvgEn,'descend');
+% b=polyfit(x,AvgEn,4);% 3 or 4进行6次拟合，b是多项式前面的值。就如2次拟合中y=ax+b,a,b的值。
+% b=polyfit(x,interp1(x,y,x),6);
+% yy=polyval(b,x);%得到拟合后y的新值
+plot(x,y,'b-')%画拟合图 
 title('平均电压趋势');
 xlabel('时间');
 ylabel('平均电压');
