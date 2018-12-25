0c3e3f977bc5bb818bdc2710b8533dd47cc18dd8
bastamon <bastamon@sohu.com>
2018-12-25 10:10:29 +0800

    201812251010ESRCHE

--- dataanalyzer.m
+++ dataanalyzer.m
@@ -13,13 +13,22 @@ load('orig1');
 timespan = 24*day(ref(end,5)-ref(1,5))+hour(ref(end,5)-ref(1,5));
 % for i=1:length(comp(:,2))       
 %     simple=zeros(timespan+1,8);
-%     if comp(i,2)==412||comp(i,2)==436
+%     if comp(i,2)==412||comp(i,2)==436||comp(i,2)==429
 %         load('orig');
+%         flag=0;
 %         onenode = ref(ref(:,2)==comp(i,2),:);%412,436 
+%         for j=1:length(onenode(:,1))            
+%             if flag==1||day(onenode(j,5))==4&&hour(onenode(j-1,5))==12&&hour(onenode(j,5))==1
+%                 flag=1;
+%                 onenode(j,5)=onenode(j,5)+datenum(0,0,0,12,0,0);
+%             end
+%             if day(onenode(j,5))==3
+%                 onenode(j,5)=onenode(j,5)+datenum(0,0,0,12,0,0);
+%             end
+%         end
 %         load('orig1');
-%     else
-%         onenode = ref(ref(:,2)==comp(i,2),:);
 %     end
+%     load('orig1');
 %     simple(1,:)=onenode(1,:);
 %     lasttime=onenode(1,5);
 %     start=2;
@@ -42,7 +51,7 @@ timespan = 24*day(ref(end,5)-ref(1,5))+hour(ref(end,5)-ref(1,5));
 %         end
 %         j=j-1;
 %     end
-%     if i~=1&&i~=16
+%     if i~=1&&i~=16&&i~=11
 %         simple1=zeros(timespan+1,8);
 %         simple1(1,:)=simple(1,:);
 %         for n=2:length(simple(:,2))
@@ -63,20 +72,18 @@ timespan = 24*day(ref(end,5)-ref(1,5))+hour(ref(end,5)-ref(1,5));
 %     simple=[simple;onenode(end,:)];    
 %     simples(i).nodeID=simple;
 % end
-% 
-% 
-% 
-% 
 % save ( 'orig_simpled1.mat','comp', 'ref','simples');
-% clear;
+
+% 
+
+
+clear;
 load('orig_simpled1.mat');
 
 
-figure(1)
-% simples(16)=[];%去除异常
+figure('NumberTitle', 'off', 'Name', '单个节点电压变化');
 msY=0;
-xend=[];
-yend=[];
+tend=[];
 for i=1:length(simples)
     [m,~]=size(simples(i).nodeID);
     x=linspace(1,m,m)'-1;
@@ -89,8 +96,7 @@ for i=1:length(simples)
 %     b=polyfit(x,y,2);
 %     yy=polyval(b,x);
     
-    xend=[xend;x(end)];
-    yend=[yend;y(end)];
+    tend=[tend;simples(i).nodeID(1,2),x(end)];
     plot(x,y);
     hold on;
     if msY<length(y)
@@ -102,12 +108,11 @@ ylabel('电压/V');
 grid on;
 hold off;
 
-tend=[];
+
 Ematrix=[];
 counts=[];
 for i=1:length(simples)
     Ematrix=[Ematrix,[simples(i).nodeID(:,6);zeros(msY-length(simples(i).nodeID(:,6)),1)]];
-    tend=[tend;simples(i).nodeID(end,5)-simples(i).nodeID(1,5)];
 end
 for i=1:length(Ematrix)
     counts=[counts;sum((Ematrix(i,:)~=0))];
@@ -118,17 +123,17 @@ end
 
 
 
-xend=24*day(tend)+hour(tend)+minute(tend)/60;
-xend=sort(xend);
+[xend,I]=sort(tend(:,2));
 yend=linspace(length(xend),1,length(xend))';
 AvgEn=sum(Ematrix,2)./counts;
-figure(2)
+figure('NumberTitle', 'off', 'Name', 'energytrend2');
 x=linspace(1,length(AvgEn),length(AvgEn))'-1;
 AvgEn=sort(AvgEn,'descend');
 % b=polyfit(x,AvgEn,4);% 3 or 4进行6次拟合，b是多项式前面的值。就如2次拟合中y=ax+b,a,b的值。
 % b=polyfit(x,interp1(x,y,x),6);
 % yy=polyval(b,x);%得到拟合后y的新值
-plot(x,y,'b-');%画拟合图 
+plot(x,y,'b-');%画拟合图
+hold on;
 title('平均电压趋势');
 xlabel('时间/h');
 ylabel('平均电压/V');
@@ -136,10 +141,14 @@ grid on;
 hold off;
 
 
-figure(3)
+figure('NumberTitle', 'off', 'Name', 'sustainNode');
 plot(xend,yend,'b-',xend,yend,'rx');
+hold on;
+for i=1:length(xend)
+    text(xend(i),yend(i)+0.5,char(['0x0',dec2hex(tend(I(i),1))]));
+end
 title('死亡时间/h');
-xlabel('时间');
+xlabel('时间/h');
 ylabel('剩余节点数/个');
 grid on;
 hold off;
@@ -149,17 +158,59 @@ hold off;
 
 % 统计消息数量，运行时间
 packetnum=[];
-firsttime=[];
-lasttime=[];
-for i=1:length(comp)
-    onenode = ref(ref(:,2)==comp(i,2),:);
-    packetnum=[packetnum;length(onenode)*34];%byte
-    firsttime=[firsttime;onenode(1,5)];
-    lasttime=[lasttime;onenode(end,5)];
+dur=[];
+temp=[];
+for i=1:length(comp)    
+    if comp(i,2)==412||comp(i,2)==436||comp(i,2)==429
+        load('orig');
+        flag=0;
+        onenode = ref(ref(:,2)==comp(i,2),:);%412,436 
+        for j=1:length(onenode(:,1))            
+            if flag==1||day(onenode(j,5))==4&&hour(onenode(j-1,5))==12&&hour(onenode(j,5))==1
+                flag=1;
+                onenode(j,5)=onenode(j,5)+datenum(0,0,0,12,0,0);
+            end
+            if day(onenode(j,5))==3
+                onenode(j,5)=onenode(j,5)+datenum(0,0,0,12,0,0);
+            end
+        end
+        load('orig1');
+    else
+        load('orig1');
+        onenode = ref(ref(:,2)==comp(i,2),:);
+    end
+    packetnum=[packetnum;length(onenode(:,1))*34];%byte
+    dur=[dur;onenode(end,5)-onenode(1,5)];
+    temp=[temp;strcat('0x0',dec2hex(comp(i,2)))];
 end
-figure(4)
-bar(packetnum);
+
+xtick=mat2cell(temp,ones(length(comp),1),6);
+x=linspace(1,length(comp),length(comp))*10;
+
+figure('NumberTitle', 'off', 'Name', 'bytepackets');
+bar(x,packetnum,0.8,'m');
+hold on;
+xlim([5 205]);
+set(gca,'xtick',x);
+set(gca,'xticklabel',xtick);
+title('组网数据包数');
+ylabel('消息量/byte');
+xlabel('节点ID');
+grid on;
+hold off;
+
+% load firstlast;
+runningdur=60.*(24.*day(dur)+hour(dur))+minute(dur); 
 
 
-runningdur=60.*hour(lasttime-firsttime)+minute(lasttime-firsttime); 
+figure('NumberTitle', 'off', 'Name', 'sustaindur');
+barh(x,runningdur,'m');
+hold on;
+ylim([0 210]);
+set(gca,'ytick',x);
+set(gca,'yticklabel',xtick);
+title('节点生存时间统计');
+ylabel('节点ID');
+xlabel('生存时间/mins');
+hold off;
 
